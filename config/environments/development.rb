e3bb5b5fb68bff5272e337f5f3e9e19a6b49229e
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-08-25 08:25:59 +0000

    ch11.2.2 ended

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,10 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = "64cb38918035478a8b88e2eb188291d3.vfs.cloud9.us-east-2.amazonaws.com/"
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }  
 
   config.action_mailer.perform_caching = false
 
