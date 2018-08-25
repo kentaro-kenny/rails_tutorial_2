89a5ced802341e1f34eeb34073a9951b1bc4b34b
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-08-25 14:12:30 +0000

    ch 11 all ended

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example user", email: "user@example.com",
+    @user = User.new(name: "Example User", email: "user@example.com",
                       password: "foobar", password_confirmation: "foobar")
   end
   
@@ -74,7 +74,7 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "authenticated? should return false for a user with nil digest"do
-    assert_not @user.authenticated?("")
+    assert_not @user.authenticated?(:remember, "")
   end
 
 end
