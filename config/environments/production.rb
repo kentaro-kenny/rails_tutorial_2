12c82eab234c0c1a2ee81bfbfbdedc6a423a85ee
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-08-30 07:23:04 +0000

    ch11 ended

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,21 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_2_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'protected-dusk-75137.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['app106353812@heroku.com'],
+    :password       => ENV['dai5v4mg9450'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  
+  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
