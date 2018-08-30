450736a334c63f4b65f293885c2908ca4d8c8f5d
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-08-30 06:06:25 +0000

    ch11.2 ended clearly

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,7 +33,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
+  config.action_mailer.default_url_options = {host: "64cb38918035478a8b88e2eb188291d3.vfs.cloud9.us-east-2.amazonaws.com" }
+                                                      
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
