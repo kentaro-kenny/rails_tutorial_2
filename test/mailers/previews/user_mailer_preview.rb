74ad4009e36a0b74f2a85e32946782c50af522a0
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-08-31 04:37:47 +0000

    ch12.2 ended

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -13,7 +13,9 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at 
   # http://64cb38918035478a8b88e2eb188291d3.vfs.cloud9.us-east-2.amazonaws.com/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    user.reset_token = User.new_token
+    UserMailer.password_reset(user)
   end
 
 end
