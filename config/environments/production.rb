2b34b5427f9e881e5863ee43fb04e6b151392eae
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-08-31 01:09:54 +0000

    ch11 sendgrid fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['b0700809@gl.aiu.ac.jp'],
-    :password       => ENV['1232Qwew1'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
