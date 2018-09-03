33adecdccf878dd01ed672a2b3e52e0533ce9207
Kentaro Nakano <b0700809@gl.aiu.ac.jp>
2018-09-03 12:38:42 +0000

    ch14.2 ended

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -15,10 +15,19 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'h1>img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination'
-    @user.microposts.paginate(page: 1).each do |micropost|
+    @user.microposts.paginate(page: 1) do |micropost|
       assert_match micropost.content, response.body
     end
     assert_select 'div.pagination', count:1
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
+  end  
+  
+  test "count relationships" do
+    log_in_as(@user)
+    get root_path
+    assert_match @user.active_relationships.count.to_s, response.body
+    assert_match @user.passive_relationships.count.to_s, response.body
   end
 
 end
